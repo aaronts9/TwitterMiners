91aef894d0c6573cd5570507ffa3ac931dcf7add
Aaron Lum <aarondlum@outlook.com>
2017-05-09 14:27:08 -0700

    Update StreamingAPI.py

--- StreamingAPI.py
+++ StreamingAPI.py
@@ -8,10 +8,10 @@ import tweepy
 import csv
 
 #pass security information to variables
-consumer_key = 'YlxV0TG53oLZaRD4OXD1tJDv3'
-consumer_secret = 'Vsc17DzrqCN0JGhyKcXNp0fNGacQRlotunmchR8OoiHeZ4Wa0T'
-access_key = '241757359-o4sxgeAzP5U18hQNT5phACNfmpiOGVl2NZUPbE3x'
-access_secret = 'SRzKmT0HlkV6UsXl3YcUMaS0yxTQ9VRA79i4dBPJJBIvg'
+consumer_key =    #   YOUR KEY
+consumer_secret = #   YOUR SECRET
+access_key =      #   YOUR KEY
+access_secret =   #   YOUR SECRET
 
 
 #use variables to access twitter
@@ -25,7 +25,7 @@ class CustomStreamListener(tweepy.StreamListener):
     def on_status(self, status): 
         '''RENAME FILE EVERY TIME AND ALSO TRACK FILTER'''
         '''Parameter'a' may need to be changed to 'wb' if python 2.7 and back to 'a' if python 3'''
-        with open('hotep.csv', 'a') as f: 
+        with open('YOURCSV.csv', 'a') as f: 
             writer = csv.writer(f)
             print(status.text)
             writer.writerow([status.author.screen_name.encode('utf-8').strip(), status.created_at, status.text.encode('utf-8').strip()])
@@ -41,4 +41,4 @@ class CustomStreamListener(tweepy.StreamListener):
 
 streamingAPI = tweepy.streaming.Stream(auth, CustomStreamListener())
 '''RENAME TRACK FILTER'''
-streamingAPI.filter(track=['hotep'], languages= ['en'])
+streamingAPI.filter(track=['YOUR TERM'], languages= ['en'])
